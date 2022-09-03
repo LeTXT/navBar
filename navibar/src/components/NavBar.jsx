40613053500bcd7d4efb15869bc5c96d1516a7b2
Leandro Gomes de Oliveira <LeTXT>
2022-09-03 19:37:04 -0300

    add useRef method

--- navibar/src/components/NavBar.jsx
+++ navibar/src/components/NavBar.jsx
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import logo from '../img/logoNav.svg'
 
 import { handleClickNav } from '../util/navBar.js'
 
 export default function NavBar() {
+    const burguer = useRef()
+    const list = useRef()
+    const li = useRef()
+
     return (
         <>
             <div className="navBar">
                 <div className="logo">
                     <img src={logo} alt="" />
                 </div>
-                <div className="burguer" onClick={() => handleClickNav()}>
+                <div className="burguer" ref={burguer} onClick={() => handleClickNav(burguer.current, list.current, li.current)}>
                     <div className="navB1"></div>
                     <div className="navB2"></div>
                     <div className="navB3"></div>
                 </div>
-                <div className="list">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Services</li>
-                    <li>Contact</li>
+                <div className="list" ref={list}>
+                <ul ref={li}>
+                    <li >Home</li>
+                    <li >About</li>
+                    <li >Services</li>
+                    <li >Contact</li>
                 </ul>
                 </div>
             </div>        
--- navibar/src/util/navBar.js
+++ navibar/src/util/navBar.js
@@ -1,14 +1,13 @@
+    
 
-export const handleClickNav = () => {
-    const list = document.querySelector('.list')
-    const li = document.querySelectorAll('li')
-    const burguer = document.querySelector('.burguer')
+export function handleClickNav(burguer, list, li) {
 
     list.classList.toggle('active')
     
-    li.forEach((link, index) => {
+    li.childNodes.forEach((link, index) => {
         link.style.animation ? (link.style.animation = '') : (link.style.animation = `fade 0.7s ease forwards ${index / 7 + 0.3}s`)
     })
+    
 
     burguer.classList.toggle('active')
 
